deafbe2531d94ef0b490c97b37ce0e3fa674ee62
Nurul Istika <nurulistika92@gmail.com>
2023-11-11 23:46:22 +0700

    Membuat query menghapus table

---
+++ Delete Table/delete_table.sql
@@ -0,0 +1,11 @@
+-- syntax menghapus table carts
+DROP TABLE carts;
+
+-- syntax menghapus table products
+DROP TABLE products;
+
+-- syntax menghapus table customers
+DROP TABLE customers;
+
+-- syntax menghapus table categories
+DROP TABLE categories;
